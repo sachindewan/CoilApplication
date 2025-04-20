28659e4d22ad2298ec523518b40229d40fdfacdf
sachin kumar <sachindewan12@gmail.com>
2025-04-20 10:17:54 +0530

    implemented  get all product

---
+++ Features/Product/GetAllProduct.cs
@@ -0,0 +1,43 @@
+﻿using Carter;
+using Coil.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coil.Api.Features.Product
+{
+    public class GetAllProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (CoilApplicationDbContext dbContext, int pageNumber = 1, int pageSize = 10) =>
+            {
+                if (pageNumber < 1 || pageSize < 1)
+                {
+                    return Results.BadRequest("Page number and page size must be greater than 0.");
+                }
+
+                var totalProducts = await dbContext.Products.CountAsync();
+                var totalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+
+                var products = await dbContext.Products
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                var response = new
+                {
+                    TotalCount = totalProducts,
+                    PageSize = pageSize,
+                    CurrentPage = pageNumber,
+                    TotalPages = totalPages,
+                    Products = products
+                };
+
+                return Results.Ok(response);
+            }).WithTags("CoilApi")
+                .WithName("GetAllProduct")
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithOpenApi();
+        }
+    }
+}
