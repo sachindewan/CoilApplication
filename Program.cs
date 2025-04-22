a09f1665afff6b74cc9aa6b82fa40998c5e3db96
sachin kumar <sachindewan12@gmail.com>
2025-04-22 08:04:09 +0530

    implemented cors policy

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 using Carter;
 using Coil.Api.Extentions;
-using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterApplicationServices();
 builder.Services.RegisterPersistenceServices(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,14 +19,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 app.UseExceptionHandler();
 app.UseStaticFiles();
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "ProductUploads")),
-//    RequestPath = "/ProductUploads"
-//});
 app.MapGroup("api/auth").WithTags("Identity.Apis").MapIdentityApi<IdentityUser>();
 app.MapCarter();
 app.Run();
